0b9a3a67eb3a3f3e3e29b85480b0e587f0d67487
Ublim <ublimjo@gmail.com>
2018-09-06 16:46:42 +0300

    Update text field

--- src/pyfield/field/text.py
+++ src/pyfield/field/text.py
@@ -1,14 +1,36 @@
+"""
+Module to hold text field
+"""
+
 from pyfield.field import Field
 
 
 class Text(Field):
+    """
+    Text field
+
+    >>> from pyfield import Text
+    >>> text = Text('First name')
+    >>> text('Jonatana')
+    >>> print(text.get)
+    Jonatana
+    """
+
     def __init__(self, name, **kwargs):
         self.name = name
         self.hold = ''
 
     def __call__(self, _input):
-        self.hold = _input
+        """
+        Inject value
+        """
+
+        self.hold = str(_input)
 
     @property
     def get(self):
+        """
+        Get holded value
+        """
+
         return self.hold
