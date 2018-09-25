579758eb335311435d2ff36bd9afb52856a747bd
Ublimjo <ublimjo@gmail.com>
2018-09-25 11:35:39 +0300

    remove expynent req

--- src/pyfield/validator/text.py
+++ src/pyfield/validator/text.py
@@ -3,7 +3,6 @@ Builtin validator for Text field
 """
 
 import re
-from expynent import patterns
 
 from pyfield.error import InvalidError
 
@@ -13,7 +12,8 @@ def is_email_addr(arg):
     Check if arg is an email
     """
 
-    email_pattern = patterns.EMAIL_ADDRESS
+    email_pattern = r"([A-Za-z0-9!#$%&'*+\/=?^_`{|.}~-]+@(?:[\
+        a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"
 
     if not re.match(email_pattern, arg):
         raise InvalidError('Invalid Email')
